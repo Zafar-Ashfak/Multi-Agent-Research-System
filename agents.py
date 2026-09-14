784277c15a8554be2b9a9582e7d52bbc14de0e5a
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-09-14 21:59:51 +0530

    Build reader agent, writer, and critic chains

--- agents.py
+++ agents.py
@@ -29,3 +29,57 @@ def build_reader_agent():
         tools=[scrape_website]
     )
 
+# Creating writer_chain
+writer_prompt = ChatPromptTemplate([
+    ("system", "You are an expert research writer. Write clear, structured and insightful reports."),
+    ("human", """
+    Write a detailed research report on the topic below.
+    
+    Topics: {topic}
+    
+    Research Gathered:
+    {research}
+    
+    Structure the report as:
+    - Introduction
+    - Key Findings (minimum 3 well-explained points)
+    - Conclusion 
+    - Sources (list all URLs found in the research)
+    
+    Be detailed, factual and professional.
+    """)
+])
+
+writer_chain = (writer_prompt
+               | llm
+               | StrOutputParser())
+
+# creating a critic_chain
+critic_prompt = ChatPromptTemplate([
+    ("system", "You are a sharp and constructive research critic. Be honest and specific."),
+    ("human", """
+    Review the research report below and evaluate it strictly.
+    Report:
+    {report}
+    
+    Respond in this exact format:
+    
+    Score: X/10
+    - ...
+    - ...
+    
+    Areas to Improve:
+    - ...
+    - ...
+    
+    One line verdict:
+    ...
+    """)
+])
+
+critic_chain = (
+    critic_prompt
+    | llm
+    | StrOutputParser()
+)
+
