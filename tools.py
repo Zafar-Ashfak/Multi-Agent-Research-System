08780ddabaa853b083dffa19c13f3fb7ab67fc79
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-09-14 18:18:46 +0530

    Create a tavily tool to search info on the web

--- tools.py
+++ tools.py
@@ -1,1 +1,24 @@
 from langchain.tools import tool
+import requests
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os
+from rich import print
+
+from dotenv import load_dotenv
+load_dotenv()
+
+tavily = TavilyClient(api_key=os.getenv('TAVILY_API_KEY'))
+
+# Creating tool
+@tool
+def web_search(query : str) -> str:
+    """
+    Search the web for recent and reliable information in a topic.
+    Return Titles, URLs, and Snippets.
+    """
+
+    results = tavily.search(query=query, max_results=5)
+    return results
+
+print(web_search.invoke('What are the recent news of America and Iran war?'))
